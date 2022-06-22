fefc04f4d8a9a014809e15bacf6077bcaf3def06
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-06-22 15:19:35 +0100

    Implemented search on employees

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -7,6 +7,7 @@ import {
   Search,
   Edit,
   Inject,
+  Toolbar
 } from "@syncfusion/ej2-react-grids";
 import { employeesData, employeesGrid, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
@@ -15,13 +16,13 @@ const Employees = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Employees" />
-      <GridComponent id="gridcomp" dataSource={employeesData} locale="en-GB" allowPaging allowSorting>
+      <GridComponent dataSource={employeesData} locale="en-GB" allowPaging allowSorting toolbar={['Search']} width='auto'>
         <ColumnsDirective>
           {employeesGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[Page, Search]} />
+        <Inject services={[Page, Search, Toolbar]} />
       </GridComponent>
     </div>
   );
